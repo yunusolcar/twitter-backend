fa2101ef15557596c6d4fde31b2074fb5a416e6c
yunus <yunusolcar@hotmail.com>
2024-06-09 10:25:00 +0300

    feat: soft delete

--- apps/tweets/views.py
+++ apps/tweets/views.py
@@ -1,4 +1,5 @@
-from rest_framework import filters
+from rest_framework import filters, status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import UserProfile, Tweet, Hashtag
@@ -28,4 +29,19 @@ class TweetModelViewSet(ModelViewSet):
         hashtag_name = self.request.query_params.get('hashtags')
         if hashtag_name:
             queryset = queryset.filter(hashtags__name=hashtag_name)
-        return queryset
+        return queryset.filter(is_deleted=False)
+
+    """
+    when the tweet is deleted, the is_deleted field becomes True and does not appear on the screen
+    """
+
+    def soft_delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        delete_tweet = request.data.get('delete_tweet')
+        if delete_tweet:
+            instance.is_deleted = True
+            instance.delete_tweet = delete_tweet
+            instance.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
